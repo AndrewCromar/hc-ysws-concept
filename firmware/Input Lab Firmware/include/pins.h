456ef38cbe5b5c5b6b701d4857bac76ae9b820a6
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-04-05 21:18:39 -0700

    feat: refactor and clean code a bit

---
+++ firmware/Input Lab Firmware/include/pins.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// LED
+const int LED_PIN = 27;
+
+// Left D-Pad
+const int PIN_LEFT_N = 17;
+const int PIN_LEFT_E = 18;
+const int PIN_LEFT_S = 26;
+const int PIN_LEFT_W = 16;
+
+// Right D-Pad
+const int PIN_RIGHT_N = 20;
+const int PIN_RIGHT_E = 21;
+const int PIN_RIGHT_S = 22;
+const int PIN_RIGHT_W = 19;
--- firmware/Input Lab Firmware/src/main.cpp
+++ firmware/Input Lab Firmware/src/main.cpp
@@ -1,40 +1,17 @@
 #include <Arduino.h>
+#include "buttons.h"
+#include "debug_logger.h"
 
-const int LED_PIN = 27;
-
-const int BTN_LEFT_N = 17;
-const int BTN_LEFT_E = 18;
-const int BTN_LEFT_S = 26;
-const int BTN_LEFT_W = 16;
-
-const int BTN_RIGHT_N = 20;
-const int BTN_RIGHT_E = 21;
-const int BTN_RIGHT_S = 22;
-const int BTN_RIGHT_W = 19;
-
-const int BUTTONS[] = {
-  BTN_LEFT_N, BTN_LEFT_E, BTN_LEFT_S, BTN_LEFT_W,
-  BTN_RIGHT_N, BTN_RIGHT_E, BTN_RIGHT_S, BTN_RIGHT_W
-};
-const int NUM_BUTTONS = sizeof(BUTTONS) / sizeof(BUTTONS[0]);
-
-const char* BUTTON_NAMES[] = {
-  "LEFT_N", "LEFT_E", "LEFT_S", "LEFT_W",
-  "RIGHT_N", "RIGHT_E", "RIGHT_S", "RIGHT_W"
-};
+Buttons buttons;
+DebugLogger logger;
 
 void setup() {
-  Serial.begin(115200);
-  for (int i = 0; i < NUM_BUTTONS; i++) {
-    pinMode(BUTTONS[i], INPUT_PULLUP);
-  }
+  logger.begin();
+  buttons.begin();
 }
 
 void loop() {
-  for (int i = 0; i < NUM_BUTTONS; i++) {
-    if (digitalRead(BUTTONS[i]) == LOW) {
-      Serial.println(BUTTON_NAMES[i]);
-    }
-  }
+  buttons.update();
+  logger.logButtons(buttons);
   delay(100);
 }
